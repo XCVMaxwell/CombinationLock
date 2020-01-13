51de2da4b413a6f041aed4f4f3137cc102063bd4
xcvmaxwell <maxmuir204@gmail.com>
2020-01-13 09:42:18 -0400

    Added comments to explain functions and their parameters.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 
 using namespace std;
 
+// Adds 7 items to linked list with value 0.
+// PARAM 1: Linked list that holds combination.
 void initializeLinkedList(LinkedList& list) {
     for (int i = 0; i <= 6; i++)
         list.Add(0);
 }
 
+// Loops through all linked list items and sets their value to 0.
+// PARAM 1: Linked list that holds combination.
 void resetCombination(LinkedList& list) {
     Node* currNode = list.first;
 
@@ -20,6 +24,10 @@ void resetCombination(LinkedList& list) {
     }
 }
 
+// Prints the combination from the linked list by looping through all items and cout-ing their the direction and their value.
+// Also puts a < next to the current working item.
+// PARAM 1: Linked list that holds combination.
+// PARAM 2: Index of the current working value in combination.
 void printCombination(const LinkedList& list, const int& workingIndex) {
     Node* currNode = list.first;
 
@@ -37,11 +45,18 @@ void printCombination(const LinkedList& list, const int& workingIndex) {
     }
 }
 
+// Changes and character to uppercase removing the need to account for both uppercase and lower case in my logic.
+// PARAM 1: String holding the user inputted command.
 char charToUpper(string character) {
     char* c = &character[0];
     return (c[0] >= 'a' && c[0] <= 'z') ? c[0] - 32 : c[0];
 }
 
+// Parses the user input using regex groups. Regex doesn't allow any commands other than 'QEGSDR' and no number over 49.
+// Regex also allows white space between the command and sub-command.
+// PARAM 1: User inputted command and possible sub-command.
+// PARAM 2: Variable to set value of parsed command.
+// PARAM 3: Variable to set value of parsed sub-command.
 void parseCommand(const string& str, string& command, string& subCommand) {
     regex regx("([QEGSDRqegsdr])\\s*([0-4]?\\d)?");
     smatch match;
@@ -55,6 +70,9 @@ void parseCommand(const string& str, string& command, string& subCommand) {
     }
 }
 
+// Writes combination to file.
+// PARAM 1: Linked list that holds combination.
+// PARAM 2: String that holds user inputted file name to be written to.
 void writeToFile(LinkedList& list, const string& fileName) {
     ofstream file;
     file.open(fileName);
